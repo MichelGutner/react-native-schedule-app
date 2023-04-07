8f6ba373b8bcf4fe4f105b7001b49cf2a7b36017
Michel Gutner <michel.gutner@sanar.com>
2023-04-07 15:38:40 -0300

    feat: add module resolution

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
+const path = require('path');
+
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: ['.jsx', '.tsx', '.js', '.ts', '.json'],
+        root: ['./'],
+        alias: {
+          '@': path.join(__dirname, './src'),
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
